49fe76f38b2f57d35a1b989c23dfc29596af4bb2
praparn <eva10409@gmail.com>
2018-05-27 22:40:29 +0700

    20180527224027

---
+++ Multi_Master/cleanUp_gcloud.sh
@@ -0,0 +1,9 @@
+gcloud compute instances stop kubernetes-ms1 kubernetes-ms2 kubernetes-ms3 kubernetes-wrk1 kubernetes-wrk2 kubernetes-wrk3 -q
+gcloud beta compute firewall-rules delete "kubernetes-lab-rule-internal" -q
+gcloud beta compute firewall-rules delete "kubernetes-lab-rule-sshicmp" -q
+gcloud beta compute firewall-rules delete "kubernetes-lab-rule-http" -q
+gcloud beta compute firewall-rules delete "kubernetes-lab-rule-cert" -q
+gcloud beta compute firewall-rules delete "kubernetes-lab-rule-kubernetes" -q
+gcloud compute instances delete kubernetes-ms1 kubernetes-ms2 kubernetes-ms3 kubernetes-wrk1 kubernetes-wrk2 kubernetes-wrk3 -q
+gcloud compute networks subnets delete "kubernetes-lab" -q
+gcloud compute networks delete "kubernetes-lab" -q
